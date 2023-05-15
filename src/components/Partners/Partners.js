a6f24ffcc6d553185dd0fb478bfdfb3c5c5d89c4
DevvSakib <devvsakib@gmail.com>
2023-05-15 14:12:10 +0600

    partners heading gradient

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -10,8 +10,8 @@ const Partners = () => {
   return (
 
     <div className='my-36'>
-      <div className='text-center opacity-50'>
-        <h1 className='border-b-2 text-5xl pb-3 font-semibold uppercase border-[#FFA500] inline-block'>Our Partners</h1>
+      <div className='text-center text-transparent bg-gradient-to-t from-black/40 to-white bg-clip-text'>
+        <h1 className='border-b-2 border-[#FFA500] text-7xl pb-3 font-semibold uppercase inline-block'>Our Partners</h1>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-10 place-items-center items-center my-14 justify-between'>
         {
