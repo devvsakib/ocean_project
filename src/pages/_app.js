d4768e724dacce44ae061b0fdf8cf7b2d601443b
DevvSakib <devvsakib@gmail.com>
2023-06-18 02:11:02 +0600

    app changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
-import { Html, Main } from 'next/document'
 import '../styles/globals.css'
 import { Inter } from 'next/font/google'
 import "../utils/typing"
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
     title: 'Create Next App',
